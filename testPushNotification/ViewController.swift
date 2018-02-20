7d0978e49b93454f56465678286e428a12fde3fb
Juan Jose Colin Arciniega <juan.jose.colin@gmail.com>
2018-02-20 14:49:18 -0600

    Initial Commit

---
+++ testPushNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testPushNotification
+//
+//  Created by Juan Jose Colin on 2/20/18.
+//  Copyright © 2018 Juan Jose Colin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
