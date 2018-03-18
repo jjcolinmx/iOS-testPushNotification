55ba044eb5cdd516e405eefa716d1d18e1760b0c
Juan Jose Colin <juan.jose.colin@gmail.com>
2018-03-17 22:11:40 -0600

    iOS - Test Push Notifications

--- testPushNotification/ViewController.swift
+++ testPushNotification/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
+import UserNotifications
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initNotificationSetupCheck()
+        getNotificationSettings()
     }
 
+    @IBAction func buttonNotificacionTouch(_ sender: Any) {
+        
+        print("Button was tapped")
+        
+         let mynotification = UNMutableNotificationContent()
+         mynotification.title="Danger"
+         mynotification.subtitle="Something comes"
+         mynotification.body="In need to tell you something, but first read this"
+         
+         let mynotificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let myrequest=UNNotificationRequest(identifier: "Notification1", content: mynotification, trigger: mynotificationTrigger)
+         
+         UNUserNotificationCenter.current().add(myrequest, withCompletionHandler: nil)
+ 
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+   
+    func initNotificationSetupCheck() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]){
+            (success,error) in
+            if success {
+                print("Permission Granted")
+            }else{
+                print("Permission Denied")
+            }
+            
+        }
+    }
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            print("Notification settings: \(settings)")
+        }
+    }
 }
 
